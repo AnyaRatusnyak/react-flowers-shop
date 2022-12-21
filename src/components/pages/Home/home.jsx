9a7500a25c9e87b10fedfff60f67360990665db5
AnyaRatusnyak <anyaaboyko@gmail.com>
2022-12-22 01:19:58 +0200

    added the Title component

--- src/components/pages/Home/home.jsx
+++ src/components/pages/Home/home.jsx
@@ -4,6 +4,7 @@ import { Post } from "./Post/post";
 import { GoodsCard } from "../../ui/goods/goodsCard";
 import { MainTitle } from "./MainTitle/MainTitle";
 import { Comment } from "./Comment/comment";
+import { Title } from "./Title/title";
 
 let arr = Array(4).fill({ name: "Flower's Name", price: 0 });
 let bestSellers = arr.map((elem) => (
@@ -17,9 +18,11 @@ export const Home = () => {
   return (
     <div className="container">
       <MainTitle />
-      <div>Best selers</div>
+      <Title name={"Best selers"} />
+
       <div className="row">{bestSellers}</div>
-      <div>Latest posts</div>
+      <Title name={"Latest posts"} />
+
       <div className="row">
         <div className="col-4">
           <Post />
@@ -31,7 +34,8 @@ export const Home = () => {
           <Post />
         </div>
       </div>
-      <div>Comments</div>
+      <Title name={"Comments"} />
+
       <div className="row">
         <div className="col-4">
           <Comment />
